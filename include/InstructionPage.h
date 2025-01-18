1adde2c5c3e7a018d6810e616e9206b3124cc95a
EhsanulHaqueSiam <ehsanul.siamdev@gmail.com>
2025-01-18 19:36:12 +0600

    basic fix for InstructionPage.h

--- include/InstructionPage.h
+++ include/InstructionPage.h
@@ -28,11 +28,9 @@ inline void renderText2(const std::string &text, const int x, const int y, void 
 }
 
 // Main display function
-inline void Instruction()
-{
+inline void Instruction() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glClearColor(0.2392f, 0.5843f, 0.6392f, 1.0f);
-
 
     // Clouds in the first group
     drawCloud(90.0f, 630.0f);   // First cloud in the first group
@@ -45,14 +43,13 @@ inline void Instruction()
     drawCloud(1050.0f, 620.0f); // Third cloud in the second group
     drawCloud(1100.0f, 590.0f); // Fourth cloud in the second group
 
+    // Hill
+    drawHill(50, 500, 300); // 2nd hill
+    drawHill(-30, 300, 230); // 1st hill
+    drawHill(500, 250, 300); // 1st hill
+    drawHill(900, 350, 400); // 1st hill
 
-    //Hill
-    drawHill(50, 500, 300); //2ndt hill
-    drawHill(-30, 300, 230); //1st hill
-    drawHill(500, 250, 300); //1st hill
-    drawHill(900, 350, 400); //1st hill
-
-    //water
+    // Water
     glBegin(GL_QUADS);
     glColor3ub(14, 12, 94);
     glVertex2f(0.0f, 0.0f);
@@ -61,7 +58,7 @@ inline void Instruction()
     glVertex2f(0.0f, 100.0f);
     glEnd();
 
-    //middle water
+    // Middle water
     glBegin(GL_QUADS);
     glColor3ub(14, 12, 94);
     glVertex2f(250.0f, 100.0f);
@@ -70,7 +67,7 @@ inline void Instruction()
     glVertex2f(0.0f, 200.0f);
     glEnd();
 
-    //Bricks
+    // Bricks
     glBegin(GL_QUADS);
     glColor3ub(114, 42, 37);
     glVertex2f(0.0f, 100.0f);
@@ -78,11 +75,11 @@ inline void Instruction()
     glVertex2f(250.0f, 200.0f);
     glVertex2f(0.0f, 200.0f);
     glEnd();
-    //draw bricks
+    // Draw bricks
     drawBorder(0, 100, 250, 200, 3.0f);
     drawBrickLines(0, 100, 250, 200, 50, 50, 3);
 
-    //2nd Bricks
+    // 2nd Bricks
     glBegin(GL_QUADS);
     glColor3ub(114, 42, 37);
     glVertex2f(400.0f, 100.0f);
@@ -90,22 +87,21 @@ inline void Instruction()
     glVertex2f(1300.0f, 200.0f);
     glVertex2f(400.0f, 200.0f);
     glEnd();
-    //draw bricks
+    // Draw bricks
     drawBorder(400, 100, 1300, 200, 3.0f);
     drawBrickLines(400, 100, 1300, 200, 50, 50, 3);
 
-    //Middle box
+    // Middle box
     glBegin(GL_QUADS);
     glColor3ub(195, 78, 25);
     glVertex2f(350.0f, 130.0f);
     glVertex2f(890.0f, 130.0f);
     glVertex2f(890.0f, 700.0f);
     glVertex2f(350.0f, 700.0f);
-
     glEnd();
     drawBorder(350, 130, 890, 700, 3.0f);
 
-    //Text Title
+    // Text Title
     renderText2("Mario Multiverse Adventure: Quick Guide", 410, 660, GLUT_BITMAP_TIMES_ROMAN_24, 1.0f, 1.0f, 1.0f);
     glBegin(GL_LINES);
     glColor3ub(255, 255, 255);
@@ -113,14 +109,13 @@ inline void Instruction()
     glVertex2f(828.0f, 650.0f);
     glEnd();
 
-    //Text Stage 1
+    // Text Stage 1
     renderText2("Stage 1: Mario's Waterway Quest", 450, 620, GLUT_BITMAP_TIMES_ROMAN_24, 1.0f, 1.0f, 1.0f);
     glBegin(GL_LINES);
     glColor3ub(255, 255, 255);
     glVertex2f(450.0f, 610.0f);
     glVertex2f(780.0f, 610.0f);
     glEnd();
-
 
     renderText2("Controls:", 360, 580, GLUT_BITMAP_TIMES_ROMAN_24, 1.0f, 1.0f, 1.0f);
     renderText2("> Up Arrow: Accelerate", 360, 555, GLUT_BITMAP_HELVETICA_18, 1.0f, 1.0f, 1.0f);
@@ -130,7 +125,7 @@ inline void Instruction()
     renderText2(">Collect coins for fuel.", 630, 555, GLUT_BITMAP_HELVETICA_18, 1.0f, 1.0f, 1.0f);
     renderText2(">Watch the Red Line for health.", 630, 530, GLUT_BITMAP_HELVETICA_18, 1.0f, 1.0f, 1.0f);
 
-    //Text Stage 2
+    // Text Stage 2
     renderText2("Stage 2: Mario's Land Escape", 470, 470, GLUT_BITMAP_TIMES_ROMAN_24, 1.0f, 1.0f, 1.0f);
     glBegin(GL_LINES);
     glColor3ub(255, 255, 255);
@@ -145,7 +140,7 @@ inline void Instruction()
     renderText2("Tips:", 630, 430, GLUT_BITMAP_TIMES_ROMAN_24, 1.0f, 1.0f, 1.0f);
     renderText2(">Be careful of water.", 630, 405, GLUT_BITMAP_HELVETICA_18, 1.0f, 1.0f, 1.0f);
 
-    //Text Stage 3
+    // Text Stage 3
     renderText2("Stage 3: Mario's Cosmic Flight", 460, 320, GLUT_BITMAP_TIMES_ROMAN_24, 1.0f, 1.0f, 1.0f);
     glBegin(GL_LINES);
     glColor3ub(255, 255, 255);
@@ -158,7 +153,6 @@ inline void Instruction()
 
     renderText2("Tips:", 630, 280, GLUT_BITMAP_TIMES_ROMAN_24, 1.0f, 1.0f, 1.0f);
     renderText2(">Dodge pillars to survive.", 630, 255, GLUT_BITMAP_HELVETICA_18, 1.0f, 1.0f, 1.0f);
-
 
     // Render credits
     renderText2("Credits:", 650, 80, GLUT_BITMAP_TIMES_ROMAN_24, 1.0f, 1.0f, 1.0f);
@@ -173,16 +167,22 @@ inline void Instruction()
 
     MarioTheMan.draw();
 
-
     glEnd();
 
     glFlush();
     glutSwapBuffers();
 }
 
+// Function to handle keyboard input
+inline void handleKeyboardInputForInstructions(unsigned char key, int x, int y) {
+    if (key == 27) { // ESC key
+        glutHideWindow();
+        glutPostRedisplay(); // Call glutPostRedisplay after setting the current window
+    }
+}
+
 // Main function for instruction page
-inline int InstructionPage()
-{
+inline int InstructionPage() {
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
     glutInitWindowSize(1280, 720);
     glutCreateWindow("Instruction Page");
@@ -192,6 +192,7 @@ inline int InstructionPage()
     gluOrtho2D(0.0f, 1280.0f, 0.0f, 720.0f);
 
     glutDisplayFunc(Instruction);
+    glutKeyboardFunc(handleKeyboardInputForInstructions); // Set the keyboard callback function
     glutMainLoop();
     return 0;
 }
